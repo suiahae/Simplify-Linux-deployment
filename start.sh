ee93b7c48f0b962b5445e7579bf95863276d6568
suiahae <45323123+suiahae@users.noreply.github.com>
2020-05-29 17:52:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@
 export http_proxy=http://192.168.160.1:7890;
 export https_proxy=http://192.168.160.1:7890;
 
+export http_proxy=http://172.20.32.1:7890;
+export https_proxy=http://172.20.32.1:7890;
+
 # export http_proxy=http://192.168.43.1:1080;
 # export https_proxy=http://192.168.43.1:1080;
 
@@ -36,13 +39,13 @@ fi
 # 安装环境
 if [ "$distributor" = "$distri_ubuntu" ];
 then
-    sudo apt install git p7zip-full gnome-tweak-tool zsh wget curl;
+    sudo apt install git p7zip-full zsh wget curl;
 elif [ "$distributor" = "$distri_fedora" ];
 then
-    sudo dnf install git p7zip gnome-tweak-tool zsh wget curl make util-linux-user
+    sudo dnf install git p7zip zsh wget curl make util-linux-user
 elif [ "$distributor" = "$distri_arch" ];
 then
-    sudo pacman -S git p7zip gnome-tweak-tool zsh wget curl;
+    sudo pacman -S git p7zip zsh wget curl;
 fi
 
 # 安装 proxychains
@@ -53,6 +56,16 @@ sudo echo "http\t192.168.43.1\t1080" >> /etc/proxychains.conf;
 sed -i '$a alias pycs=proxychains4' ~/.bashrc;
 
 # 安装主题
+if [ "$distributor" = "$distri_ubuntu" ];
+then
+    sudo apt install gnome-tweak-tool;
+elif [ "$distributor" = "$distri_fedora" ];
+then
+    sudo dnf install gnome-tweak-tool;
+elif [ "$distributor" = "$distri_arch" ];
+then
+    sudo pacman -S gnome-tweak-tool;
+fi
 ./scripts/update-Qogir-theme-online.sh;
 ./scripts/update-Vimix-gtk-themes-online.sh;
 
