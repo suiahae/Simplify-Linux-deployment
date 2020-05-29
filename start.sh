6d8d898dd6cd1abef1afee6544d2da003e2972a0
suiahae <45323123+suiahae@users.noreply.github.com>
2020-05-29 20:14:34 +0800

    Update start.sh
    采用变量分离代理地址

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 # https://mirrors.tuna.tsinghua.edu.cn/
 # https://extensions.gnome.org/
 # https://rpmfusion.org/Howto/NVIDIA
+proxytype="http";
+proxyhost="192.168.160.1";
+proxyport="7890";
+proxyaddress=$proxytype"://"$proxyhost":"$proxyport;
 
-export http_proxy=http://192.168.160.1:7890;
-export https_proxy=http://192.168.160.1:7890;
 
-export http_proxy=http://172.20.32.1:7890;
-export https_proxy=http://172.20.32.1:7890;
+export http_proxy=$proxyaddress;
+export https_proxy=$proxyaddress;
 
 # export http_proxy=http://192.168.43.1:1080;
 # export https_proxy=http://192.168.43.1:1080;
@@ -37,35 +39,24 @@ then
 fi
 
 # 安装环境
-if [ "$distributor" = "$distri_ubuntu" ];
-then
-    sudo apt install git p7zip-full zsh wget curl;
-elif [ "$distributor" = "$distri_fedora" ];
-then
-    sudo dnf install git p7zip zsh wget curl make util-linux-user
-elif [ "$distributor" = "$distri_arch" ];
-then
-    sudo pacman -S git p7zip zsh wget curl;
-fi
+
+sudo apt install git p7zip-full zsh wget curl make;
+sudo dnf install git p7zip zsh wget curl make util-linux-user
+sudo pacman -S git p7zip zsh wget curl make;
 
 # 安装 proxychains
 ./scripts/install-proxychains.sh;
 # 更改 proxychains 代理
-sudo sed -i 's/^socks.*/http\t192.168.160.1\t7890/g' /etc/proxychains.conf;
-sudo echo "http\t192.168.43.1\t1080" >> /etc/proxychains.conf;
-sed -i '$a alias pycs=proxychains4' ~/.bashrc;
+sudo sed -i "s/^socks.*/http\t192.168.160.1\t7890/g" /etc/proxychains.conf;
+sudo sed -i "s/^socks.*/$proxytype\t$proxyhost\t$proxyport/g" /etc/proxychains.conf;
+#sudo echo "http\t192.168.43.1\t1080" >> /etc/proxychains.conf;
+echo "alias pycs=proxychains4" >> ~/.bashrc;
 
 # 安装主题
-if [ "$distributor" = "$distri_ubuntu" ];
-then
-    sudo apt install gnome-tweak-tool;
-elif [ "$distributor" = "$distri_fedora" ];
-then
-    sudo dnf install gnome-tweak-tool;
-elif [ "$distributor" = "$distri_arch" ];
-then
-    sudo pacman -S gnome-tweak-tool;
-fi
+sudo apt install gnome-tweak-tool;
+sudo dnf install gnome-tweak-tool;
+sudo pacman -S gnome-tweak-tool;
+
 ./scripts/update-Qogir-theme-online.sh;
 ./scripts/update-Vimix-gtk-themes-online.sh;
 
@@ -79,4 +70,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # 更改 plugins 配置
 sed -i 's/plugins=(.*/plugins=(vim-interaction pip git sudo extract z wd archlinux zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc;
 sed -i 's/ZSH_THEME=".*/ZSH_THEME="ys"/g' ~/.zshrc;
-sed -i '$a alias pycs=proxychains4' ~/.zshrc;
+echo "alias pycs=proxychains4" >> ~/.zshrc;
