9a48c7290a48532216f41b531cab1460a031da83
suiahae <45323123+suiahae@users.noreply.github.com>
2020-12-07 16:41:52 +0800

    update Qogir-icon-theme to 2020-11-22

--- scripts/update-Qogir-icon-online.sh
+++ scripts/update-Qogir-icon-online.sh
@@ -9,11 +9,11 @@
 cd /tmp;
 rm -Rf Qogir-icon-theme.zip 2>/dev/null; 
 rm -Rf Qogir-icon-theme-master/ 2>/dev/null; 
-wget https://github.com/vinceliuice/Qogir-icon-theme/archive/2020-06-22.zip -O Qogir-icon-theme.zip; 
+wget https://github.com/vinceliuice/Qogir-icon-theme/archive/2020-11-22.zip -O Qogir-icon-theme.zip; 
 7za x Qogir-icon-theme.zip; 
-cd Qogir-icon-theme-2020-06-22; 
+cd Qogir-icon-theme-2020-11-22; 
 ./install.sh; 
 cd ..;
 rm -Rf Qogir-icon-theme.zip 2>/dev/null; 
-rm -Rf Qogir-icon-theme-2020-06-22/ 2>/dev/null; 
+rm -Rf Qogir-icon-theme-2020-11-22/ 2>/dev/null; 
 # notify-send "All done!" "Enjoy the latest version of Qogir!" -i face-smile
