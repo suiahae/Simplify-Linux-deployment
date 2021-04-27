764914bf11d2882210393ed06d11d6f1e1bbcf7d
suiahae <45323123+suiahae@users.noreply.github.com>
2021-04-27 11:18:42 +0800

    update Qogir-theme to 2021-04-20 that supports gnome-shell 40.0

--- scripts/update-Qogir-theme-online.sh
+++ scripts/update-Qogir-theme-online.sh
@@ -9,11 +9,11 @@
 cd /tmp;
 rm -Rf Qogir-theme.zip 2>/dev/null; 
 rm -Rf Qogir-theme-master/ 2>/dev/null; 
-wget https://github.com/vinceliuice/Qogir-theme/archive/2020-11-16.zip -O Qogir-theme.zip; 
+wget https://github.com/vinceliuice/Qogir-theme/archive/refs/tags/2021-04-20.zip -O Qogir-theme.zip; 
 7za x Qogir-theme.zip; 
-cd Qogir-theme-2020-11-16; 
+cd Qogir-theme-2021-04-20; 
 ./install.sh; 
 cd ..;
 rm -Rf Qogir-theme.zip 2>/dev/null; 
-rm -Rf Qogir-theme-2020-11-16/ 2>/dev/null; 
+rm -Rf Qogir-theme-2021-04-20/ 2>/dev/null; 
 # notify-send "All done!" "Enjoy the latest version of Qogir!" -i face-smile
