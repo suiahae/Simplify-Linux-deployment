c73b2da160e493bd7c6367e32f6c24c880c7ff6a
suiahae <45323123+suiahae@users.noreply.github.com>
2023-02-14 21:42:25 +0800

    Update update-Vimix-gtk-themes-online.sh

--- scripts/update-Vimix-gtk-themes-online.sh
+++ scripts/update-Vimix-gtk-themes-online.sh
@@ -9,11 +9,11 @@
 cd /tmp;
 rm -Rf vimix-gtk-themes.zip 2>/dev/null; 
 rm -Rf vimix-gtk-themes-master/ 2>/dev/null; 
-wget https://github.com/vinceliuice/vimix-gtk-themes/archive/refs/tags/2021-04-25.zip -O vimix-gtk-themes.zip; 
+wget https://github.com/vinceliuice/vimix-gtk-themes/archive/refs/tags/2023-01-25.zip -O vimix-gtk-themes.zip; 
 7za x vimix-gtk-themes.zip; 
-cd vimix-gtk-themes-2021-04-25; 
-./install.sh; 
+cd vimix-gtk-themes-2023-01-25; 
+./install.sh -t all -s all; 
 cd ..;
 rm -Rf vimix-gtk-themes.zip 2>/dev/null; 
-rm -Rf vimix-gtk-themes-2021-04-25/ 2>/dev/null; 
+rm -Rf vimix-gtk-themes-2023-01-25/ 2>/dev/null; 
 # notify-send "All done!" "Enjoy the latest version of Vimix!" -i face-smile
