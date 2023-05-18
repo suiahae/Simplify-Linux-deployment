ed60800d89ab5573984c906b94c16bf85c934bb2
suiahae <45323123+suiahae@users.noreply.github.com>
2023-05-18 20:10:48 +0800

    Create update- vimix-icon-online.sh

---
+++ scripts/update- vimix-icon-online.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# notify-send "Vimix Icon Theme" "Getting the latest version of the Vimix Icon Theme..." -i system-software-update; 
+# sudo dnf install p7zip
+# sudo apt install p7zip
+# sudo pacman -S p7zip-full
+
+VGT_VERSION="2023-01-18";
+
+# mkdir .tmp 2>/dev/null; 
+# cd .tmp; 
+cd /tmp;
+rm -Rf vimix-icon-theme.zip 2>/dev/null; 
+rm -Rf vimix-icon-theme-master/ 2>/dev/null; 
+wget https://github.com/vinceliuice/vimix-icon-theme/archive/refs/tags/$VGT_VERSION.zip -O vimix-icon-theme.zip; 
+7za x vimix-icon-theme.zip; 
+cd vimix-icon-theme-*; 
+./install.sh -n doder; 
+cd ..;
+rm -Rf vimix-icon-theme.zip 2>/dev/null; 
+rm -Rf vimix-icon-theme-*/ 2>/dev/null; 
+# notify-send "All done!" "Enjoy the latest version of Vimix!" -i face-smile
