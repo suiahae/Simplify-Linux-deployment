fc4109eb42991734e2df0dd0bf40f033c833561c
suiahae <45323123+suiahae@users.noreply.github.com>
2023-05-18 20:20:20 +0800

    Update update-vimix-icon-online.sh

--- scripts/update-vimix-icon-online.sh
+++ scripts/update-vimix-icon-online.sh
@@ -9,13 +9,11 @@ VIT_VERSION="2023-01-18";
 # mkdir .tmp 2>/dev/null; 
 # cd .tmp; 
 cd /tmp;
-rm -Rf vimix-icon-theme.zip 2>/dev/null; 
-rm -Rf vimix-icon-theme-master/ 2>/dev/null; 
+rm -Rf vimix-icon-theme* 2>/dev/null; 
 wget https://github.com/vinceliuice/vimix-icon-theme/archive/refs/tags/$VIT_VERSION.zip -O vimix-icon-theme.zip; 
 7za x vimix-icon-theme.zip; 
 cd vimix-icon-theme-*; 
 ./install.sh -n doder; 
 cd ..;
-rm -Rf vimix-icon-theme.zip 2>/dev/null; 
-rm -Rf vimix-icon-theme-*/ 2>/dev/null; 
+rm -Rf vimix-icon-theme* 2>/dev/null; 
 # notify-send "All done!" "Enjoy the latest version of Vimix!" -i face-smile
