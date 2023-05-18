e109ea98d20819e50c54e9cb2c4d3de7a7027db6
suiahae <45323123+suiahae@users.noreply.github.com>
2023-05-18 20:19:02 +0800

    Update update-Qogir-icon-online.sh

--- scripts/update-Qogir-icon-online.sh
+++ scripts/update-Qogir-icon-online.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# notify-send "Qogir-icon-theme" "Getting the latest version of the Qogir icon theme..." -i system-software-update; 
+# notify-send "Qogir Icon Theme" "Getting the latest version of the Qogir Icon Theme..." -i system-software-update; 
 # sudo dnf install p7zip
 # sudo apt install p7zip
 # sudo pacman -S p7zip-full
 
+VIT_VERSION="2023-02-23";
+
 # mkdir .tmp 2>/dev/null; 
 # cd .tmp; 
 cd /tmp;
-rm -Rf Qogir-icon-theme.zip 2>/dev/null; 
-rm -Rf Qogir-icon-theme-master/ 2>/dev/null; 
-wget https://github.com/vinceliuice/Qogir-icon-theme/archive/2020-11-22.zip -O Qogir-icon-theme.zip; 
+rm -Rf Qogir-icon-theme* 2>/dev/null; 
+wget https://github.com/vinceliuice/Qogir-icon-theme/archive/refs/tags/$VIT_VERSION.zip -O Qogir-icon-theme.zip; 
 7za x Qogir-icon-theme.zip; 
-cd Qogir-icon-theme-2020-11-22; 
+cd Qogir-icon-theme-*; 
 ./install.sh; 
 cd ..;
-rm -Rf Qogir-icon-theme.zip 2>/dev/null; 
-rm -Rf Qogir-icon-theme-2020-11-22/ 2>/dev/null; 
+rm -Rf Qogir-icon-theme* 2>/dev/null; 
 # notify-send "All done!" "Enjoy the latest version of Qogir!" -i face-smile
