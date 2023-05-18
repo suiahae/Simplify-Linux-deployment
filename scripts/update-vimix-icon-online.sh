190cb95fec698dfb25c50f8f187a3b6cee7641c0
suiahae <45323123+suiahae@users.noreply.github.com>
2023-05-18 20:14:00 +0800

    Update update-vimix-icon-online.sh

--- scripts/update-vimix-icon-online.sh
+++ scripts/update-vimix-icon-online.sh
@@ -4,14 +4,14 @@
 # sudo apt install p7zip
 # sudo pacman -S p7zip-full
 
-VGT_VERSION="2023-01-18";
+VIT_VERSION="2023-01-18";
 
 # mkdir .tmp 2>/dev/null; 
 # cd .tmp; 
 cd /tmp;
 rm -Rf vimix-icon-theme.zip 2>/dev/null; 
 rm -Rf vimix-icon-theme-master/ 2>/dev/null; 
-wget https://github.com/vinceliuice/vimix-icon-theme/archive/refs/tags/$VGT_VERSION.zip -O vimix-icon-theme.zip; 
+wget https://github.com/vinceliuice/vimix-icon-theme/archive/refs/tags/$VIT_VERSION.zip -O vimix-icon-theme.zip; 
 7za x vimix-icon-theme.zip; 
 cd vimix-icon-theme-*; 
 ./install.sh -n doder; 
