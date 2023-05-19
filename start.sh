4f209f8e2c8581e5b9f098f7a5f662206ed1f3e5
suiahae <45323123+suiahae@users.noreply.github.com>
2023-05-19 11:21:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -61,13 +61,17 @@ sudo dnf install gnome-extensions-app gnome-tweak-tool p7zip wget -y 2>/dev/null
 
 # proxychains4 ./scripts/update-Qogir-theme-online.sh;
 proxychains ./scripts/update-Qogir-icon-online.sh;
-proxychains ./scripts/update-Vimix-gtk-themes-online.sh;
+proxychains ./scripts/update-vimix-gtk-themes-online.sh;
 
 ## 在 Flatpak 应用程序上应用 GTK 主题 [[itsfoss](https://itsfoss.com/flatpak-app-apply-theme/)]，例如：
-sudo flatpak override --filesystem=$HOME/.themes
+sudo flatpak override --filesystem=$HOME/.local/share/themes
 sudo flatpak override --filesystem=$HOME/.local/share/icons
 sudo flatpak override --env=GTK_THEME=vimix-light-doder
 sudo flatpak override --env=ICON_THEME=Qogir
+
+# 恢复默认
+# sudo flatpak override --reset
+
 
 # 安装环境
 sudo apt install git zsh wget -y 2>/dev/null; 
